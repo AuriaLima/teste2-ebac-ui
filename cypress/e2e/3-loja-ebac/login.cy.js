abccfd5c6167bc31c36f330b3a744ee9eac97335
Auria Maria <auria.limabs@gmail.com>
2024-02-22 11:28:25 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,15 +1,37 @@
 /// <reference types="cypress"/>
 
 describe('Funcionalidade: Login',() => {
-    it('Deve fazer login com sucesso', () => {
+
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('auria.limabs@gmail.com')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, auria.limabs (não é auria.limabs? Sair)')
 
-        
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir o usuário inválido', () => {
+        cy.get('#username').type('auria@gmail.com')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('auria.limabs@gmail.com')
+        cy.get('#password').type('teste123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain','Erro: A senha fornecida para o e-mail auria.limabs@gmail.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('exist')
     });
     
 
