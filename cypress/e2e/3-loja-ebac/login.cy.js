219deb363f587e5ebcd2704b7fa85a4a9f0f8d6d
Auria Maria <auria.limabs@gmail.com>
2024-02-23 12:17:59 -0300

    inserção de arquivo de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,10 +1,11 @@
 /// <reference types="cypress"/>
+const perfil =require('../../fixtures/perfil.json')
 
-describe('Funcionalidade: Login',() => {
+describe('Funcionalidade: Login', () => {
 
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
     afterEach(() => {
         cy.screenshot()
@@ -30,9 +31,25 @@ describe('Funcionalidade: Login',() => {
         cy.get('#username').type('auria.limabs@gmail.com')
         cy.get('#password').type('teste123')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain','Erro: A senha fornecida para o e-mail auria.limabs@gmail.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail auria.limabs@gmail.com está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
     });
-    
+
+    it('Deve fazer lgin com sucesso usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, auria.limabs (não é auria.limabs? Sair)')
+    });
+
+    it.only('Deve fazer lgin com sucesso usando Fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha,{log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, auria.limabs (não é auria.limabs? Sair)') 
+        })
+    });
+
 
 })
