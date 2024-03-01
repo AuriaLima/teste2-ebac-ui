ca3d2c9a974585854d57dea116a1370e9fbbb532
Auria Maria <auria.limabs@gmail.com>
2024-02-29 21:45:18 -0300

    Page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,22 +1,33 @@
 /// <reference types="cypress"/>
-
+import produtosPages from "../../support/pages.objects/produtos.pages";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPages.visitaUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-        //.first()
-        .contains('Abominable Hoodie')
-        .click()
+        produtosPages.buscarProdutoLista('Abominable Hoodie')
 
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
-        
-    });
-    
 
-    
+    });
+
+    it.only('Deve buscar produto com sucesso', () => {
+        let produto = 'Ariel Roll Sleeve Sweatshirt'
+        produtosPages.buscarProdutos(produto)
+        cy.get('.product_title').should('contain', produto)
+
+    });
+
+    it('Deve visitar a página do produto', () => {
+
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+
+    });
+
+
 });
