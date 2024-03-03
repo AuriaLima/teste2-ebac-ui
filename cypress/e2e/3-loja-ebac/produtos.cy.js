835f238888c211527db1efa97b34b797c17c145a
Auria Maria <auria.limabs@gmail.com>
2024-03-02 21:37:27 -0300

    Page Object

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -14,7 +14,7 @@ describe('Funcionalidade: Produtos', () => {
 
     });
 
-    it.only('Deve buscar produto com sucesso', () => {
+    it('Deve buscar produto com sucesso', () => {
         let produto = 'Ariel Roll Sleeve Sweatshirt'
         produtosPages.buscarProdutos(produto)
         cy.get('.product_title').should('contain', produto)
@@ -22,12 +22,29 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve visitar a página do produto', () => {
+        produtosPages.visitarProduto('Ariel Roll Sleeve Sweatshirt')
+        cy.get('.product_title').should('contain', 'Ariel Roll Sleeve Sweatshirt')
+
 
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 1
+        produtosPages.buscarProdutos('Hero Hoodie')
+        produtosPages.addProdutoCarrinho('M', 'Black', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' x “Hero Hoodie” foi adicionado no seu carrinho.')
 
     });
 
-
-});
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+    
+        produtosPages.buscarProdutos(dados[1].nomeProduto)
+        produtosPages.addProdutoCarrinho(
+            dados[1].tamanho, 
+            dados[1].cor, 
+            dados[1].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+        })
+})
+})
